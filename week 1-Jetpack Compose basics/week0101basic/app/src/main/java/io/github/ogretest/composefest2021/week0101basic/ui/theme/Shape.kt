05022a20f536195e91fdb316e26330cdd3d4ee12
Moon-Young, Lee <ogretest@gmail.com>
2021-11-07 08:41:27 +0900

    week0101basic - 2 Starting a new Compose project

---
+++ week 1-Jetpack Compose basics/week0101basic/app/src/main/java/io/github/ogretest/composefest2021/week0101basic/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package io.github.ogretest.composefest2021.week0101basic.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
