ae6decae2bb3f1122720ee9368af7a2d4fbe1e02
Moon-Young, Lee <ogretest@gmail.com>
2021-11-07 10:36:04 +0900

    week0101basic - 13 Finishing touches!

--- week 1-Jetpack Compose basics/week0101basic/app/src/main/java/io/github/ogretest/composefest2021/week0101basic/MainActivity.kt
+++ week 1-Jetpack Compose basics/week0101basic/app/src/main/java/io/github/ogretest/composefest2021/week0101basic/MainActivity.kt
@@ -4,17 +4,22 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.ogretest.composefest2021.week0101basic.ui.theme.Week0101basicTheme
@@ -79,28 +84,50 @@ fun OnboardingPreview() {
 @Composable
 fun Greeting(name: String) {
     var expanded by remember { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                ),
+        ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)),
+                    .padding(12.dp)
+
             ) {
                 Text(text = "Hello")
-                Text(text = "$name!", style = MaterialTheme.typography.h4)
+                Text(
+                    text = "$name!",
+                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+                )
+                if (expanded) {
+                    Text(
+                        ("Composem ipsum color sit lazy, padding " +
+                                "theme elit, sed do bouncy. ").repeat(4)
+                    )
+                }
             }
-            OutlinedButton(onClick = { expanded = !expanded }) {
-                Text(if (expanded) " Show less" else "Show more")
+            IconButton(onClick = { expanded = !expanded }) {
+                if (expanded) {
+                    Icon(
+                        imageVector = Icons.Filled.ExpandLess,
+                        contentDescription = stringResource(R.string.show_less),
+                    )
+                } else {
+                    Icon(
+                        imageVector = Icons.Filled.ExpandMore,
+                        contentDescription = stringResource(R.string.show_more),
+                    )
+                }
             }
         }
     }
