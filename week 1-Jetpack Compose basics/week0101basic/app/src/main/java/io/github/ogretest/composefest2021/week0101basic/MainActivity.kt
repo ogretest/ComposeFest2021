68726ad75433957afd9bc278921b1df63aeed0d1
Moon-Young, Lee <ogretest@gmail.com>
2021-11-07 09:07:57 +0900

    week0101basic - 6 Creating columns and rows

--- week 1-Jetpack Compose basics/week0101basic/app/src/main/java/io/github/ogretest/composefest2021/week0101basic/MainActivity.kt
+++ week 1-Jetpack Compose basics/week0101basic/app/src/main/java/io/github/ogretest/composefest2021/week0101basic/MainActivity.kt
@@ -3,6 +3,8 @@ package io.github.ogretest.composefest2021.week0101basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -25,24 +27,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp() {
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
     // A surface container using the 'background' color from the theme
     Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+            names.forEach { Greeting(it) }
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(24.dp),
-        )
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+    ) {
+        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
+            Text(text = "Hello")
+            Text(text = "$name!")
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     Week0101basicTheme {
