a1aa79308fb9ebec29433056c3e7673164ca7a2d
Moon-Young, Lee <ogretest@gmail.com>
2021-11-13 19:04:55 +0900

    week0201layouts - 9 Layout modifiers under the hood

--- week 2-1-Layouts in JetpackCompose/week0201layouts/app/src/main/java/io/github/ogretest/composefest2021/week0201layouts/LayoutsCodeLab.kt
+++ week 2-1-Layouts in JetpackCompose/week0201layouts/app/src/main/java/io/github/ogretest/composefest2021/week0201layouts/LayoutsCodeLab.kt
@@ -1,14 +1,17 @@
 package io.github.ogretest.composefest2021.week0201layouts
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.ogretest.composefest2021.week0201layouts.ui.theme.Week0201layoutsTheme
@@ -47,7 +50,13 @@ val topics = listOf(
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+    Row(
+        modifier = modifier
+            .background(color = Color.LightGray)
+            .padding(16.dp)
+            .size(200.dp)
+            .horizontalScroll(rememberScrollState())
+    ) {
         StaggeredGrid(modifier = modifier, rows = 5) {
             for (topic in topics) {
                 Chip(modifier = Modifier.padding(8.dp), text = topic)
