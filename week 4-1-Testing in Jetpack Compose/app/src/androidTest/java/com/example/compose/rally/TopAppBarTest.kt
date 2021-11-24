1239e2846ef4f0f26fd105810e68cc5cb949c541
Moon-Young, Lee <ogretest@gmail.com>
2021-11-24 21:34:45 +0900

    week0401testing - 3 Create a simple UI test

---
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,41 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreen = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreen,
+                onTabSelected = { /*TODO*/ },
+                currentScreen = RallyScreen.Accounts,
+            )
+        }
+        Thread.sleep(5000L)
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreen = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreen,
+                onTabSelected = { /*TODO*/ },
+                currentScreen = RallyScreen.Accounts,
+            )
+        }
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+}
